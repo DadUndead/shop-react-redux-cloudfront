5a3e9cabd4b2ff1b81ee3296be4bb7cd00dd8e79
Alexei Klimenko <Alexei_Klimenko@epam.com>
2023-02-26 22:46:34 +0100

    fix (api paths): fix typos in api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: `https://jx5a3hc7j4..execute-api.eu-west-1.amazonaws.com/dev`,
-  order: "https://jx5a3hc7j4..execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://jx5a3hc7j4..execute-api.eu-west-1.amazonaws.com/dev",
+  product: `https://jx5a3hc7j4.execute-api.eu-west-1.amazonaws.com/dev`,
+  order: "https://jx5a3hc7j4.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://jx5a3hc7j4.execute-api.eu-west-1.amazonaws.com/dev",
   bff: `https://jx5a3hc7j4.execute-api.eu-west-1.amazonaws.com/dev`,
-  cart: "https://jx5a3hc7j4..execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://jx5a3hc7j4.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
